5367d8d0ae49cffdb0e478e4dd4073ebfedba872
MorgynKass <morgynkass@gmail.com>
2024-01-19 13:07:20 -0700

    first commit

---
+++ portfolio-project/src/app/components/HeroSection.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const HeroSection = () => {
+  return (
+    <section>
+      <h2 className="text-white mb-4 text-4x1 lg:text-6xl font-extrabold">
+        Hello, I'm Morgyn
+      </h2>
+      <p className="text-[#ADB7BE]">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sed veritatis
+        hic laboriosam dolore iste cupiditate, autem similique, ut rerum iure,
+        quod provident sint repellendus. Rem, adipisci. Repellendus optio iusto
+        rerum!
+      </p>
+    </section>
+  );
+};
+
+export default HeroSection;
